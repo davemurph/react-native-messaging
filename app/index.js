30ff59d3a053ae40fbbf07aad293142972081c8b
billybob <murphy_david_t@hotmail.com>
2018-12-03 20:10:04 +0000

    fixed to preload images during app loading

--- app/index.js
+++ app/index.js
@@ -1,19 +1,52 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { YellowBox } from 'react-native'
 import { Provider } from 'react-redux'
+import { Asset, AppLoading } from 'expo';
 
 import { configureStore } from './store'
 
 import ChatApp from './components/ChatApp'
+
+Expo.Asset.fromModule(require("../app/images/form.png")).downloadAsync()
 
 console.ignoredYellowBox = ['Setting a timer']
 YellowBox.ignoreWarnings(['Setting a timer'])
 
 const store = configureStore()
 
-const App = () =>
-  <Provider store={store}>
-    <ChatApp />
-  </Provider>
+export default class App extends Component {
+  state = {
+    isReady: false,
+  }
 
-export default App
+  async _cacheResourcesAsync() {
+    const images = [
+      // TODO: Add all images/assets here to assist in load times, loading this one gets over the inital hump for now
+      require('../app/images/form.png'),
+    ];
+
+    const cacheImages = images.map((image) => {
+      return Asset.fromModule(image).downloadAsync();
+    });
+    return Promise.all(cacheImages)
+
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._cacheResourcesAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      );
+    }
+
+    return (
+      <Provider store={store}>
+        <ChatApp />
+      </Provider>
+    )
+  }
+}
